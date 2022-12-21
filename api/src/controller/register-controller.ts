0123e3fb78bc9d1d6830dd8eaf97b8af938e2020
Laurenz Honauer <laurenz.honauer@gmail.com>
2022-12-21 18:24:37 +0100

    try without host constraint

--- api/src/controller/register-controller.ts
+++ api/src/controller/register-controller.ts
@@ -1,11 +1,10 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import config from "../config";
 import { addContact } from "../service/register-service";
 
 export default async function registerController(fastify: FastifyInstance) {
   fastify.post(
     "/",
-    { constraints: { host: config.validHost } },
+    // { constraints: { host: config.validHost } },
     async function (_request: FastifyRequest, reply: FastifyReply) {
       const { email } = _request.body as any;
       const response = await addContact(email);
