493874aedbab539bbe53194fa528aa4cd306059f
Laurenz Honauer <laurenz.honauer@gmail.com>
2022-12-21 15:51:21 +0100

    Print config

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,3 +11,4 @@ app.listen({ port: config.port, path: config.host });
 
 console.log(`🚀  Fastify server running on  ${config.host}:${config.port}`);
 console.log(`Route user: /api/v1/register`);
+console.log({ ...config });
