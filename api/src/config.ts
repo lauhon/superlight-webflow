a2dff87cac34635f22607ac4b2b57f556115e6ef
Laurenz Honauer <laurenz.honauer@gmail.com>
2023-02-07 16:21:00 +0100

    Set up welcome mail sending

--- api/src/config.ts
+++ api/src/config.ts
@@ -7,8 +7,12 @@ const config = {
   mailJetContactList: process.env.MAILJET_CONTACT_LIST || "",
   mailJetUser: process.env.MAILJET_USER || "",
   mailJetPassword: process.env.MAILJET_PASSWORD || "",
+  mailJetWelcomeTemlate: Number.parseInt(
+    process.env.MAILJET_WELCOME_TEMPLATE || "0"
+  ),
   port: Number(process.env.PORT || "8080"),
   host: process.env.HOST || "0.0.0.0",
+  logLevel: process.env.LOG_LEVEL || "info",
 };
 
 export default config;
